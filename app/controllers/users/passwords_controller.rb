9304e42fc0bc023b5556e03756912b51ab0176f3
Luis Madrigal <luisgerardomg95@gmail.com>
2025-01-02 16:23:10 -0600

    fix

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  skip_before_action :assert_reset_token_passed, only: [:edit]
+  skip_before_action :require_no_authentication, only: [:edit, :update]
+  before_action :set_user
+
+
+  def new
+    # Redirige al formulario de edición directamente (o a un usuario específico si lo deseas)
+    redirect_to edit_user_password_path(reset_password_token: params[:reset_password_token])
+  end
+
+  def edit
+    Rails.logger.info "en el edit"
+    # Si el usuario está autenticado, permite editar su contraseña
+    if @user
+      self.resource = @user
+      render :edit
+    else
+      redirect_to new_user_session_path, alert: "Debes estar autenticado para cambiar tu contraseña."
+    end
+  end
+
   # GET /resource/password/new
   # def new
   #   super
@@ -17,9 +38,28 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    # Obtener al usuario actual
+    self.resource = @user
+
+    # Actualizar la contraseña sin el token
+    if resource.update(password_params)
+      bypass_sign_in(resource) # Vuelve a autenticar al usuario con la nueva contraseña
+      redirect_to '/', notice: "Tu contraseña ha sido cambiada con éxito."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation, :id)
+  end
+
+  def set_user
+    @user = User.find(params[:id] || password_params[:id])
+  end
 
   # protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def can_login?
+    profile = Profile.find_by(email: email)
+    
+    return false unless profile
+    
+    true
+  end
 end
